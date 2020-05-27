d2f50d20c9910864e2f8589478754b957a43c012
Ruben Colomina <rcolomina@gmail.com>
2020-05-27 10:39:37 +0100

    init ver

---
+++ config.py
@@ -0,0 +1,8 @@
+class Config:
+    USER_PATH = '/home/pi'
+    CONFIG_PATH = 'crypto_data_collector'
+    LOG_PATH = 'logs'
+    LOG_FILENAME = 'books_bitmex.log'
+    BASE_URI_BITMEX = 'https://testnet.bitmex.com/api/v1'
+    BOOK_DEPTH = 25
+    SYMBOL = "XBTUSD"
