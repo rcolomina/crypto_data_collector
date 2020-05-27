4a280dcd36e1a36a72d0b1d7f984ebae5c1b0eb4
Ruben Colomina <rcolomina@gmail.com>
2020-05-27 10:38:00 +0100

    added configuration object

--- collect_data_books.py
+++ collect_data_books.py
@@ -1,4 +1,5 @@
 import requests,os,time,sys
+from config import Config as conf
 
 import logging
 from logging.handlers import TimedRotatingFileHandler
@@ -16,22 +17,21 @@ logger.addHandler(console_handler)
 # SETUP LOG HANDLER ON OUTPUT FILE
 logger.setLevel(logging.DEBUG)
 
-user_path="/home/pi"
-config_path="crypto_data_collector"
-log_path="logs"
+user_path=conf.USER_PATH
+config_path=conf.CONFIG_PATH
+log_path=conf.LOG_PATH
+log_filename=conf.LOG_FILENAME
+base_uri_bitmex = conf.BASE_URI_BITMEX
 
-path=user_path+"/"+config_path+"/books_bitmex.log"
+path = user_path + "/" + config_path + "/" + log_filename
 handler = TimedRotatingFileHandler(path,when="m",interval=1)
 logger.addHandler(handler)
 
-base_uri_bitmex="https://testnet.bitmex.com/api/v1" # TODO
-
 # Request data
-def get_api_bitmex_book(depth="25"):
+def get_api_bitmex_book(depth="25",symbol="XBTUSD"):
     '''
     Calling API to perform a request againts API getting the lattest exchange order book
     '''
-    symbol = "XBTUSD"
     orderBookQuery = "/orderBook/L2?symbol={}&depth={}".format(symbol,depth)    
     order_book_uri_bitmex = "{}{}".format(base_uri_bitmex,orderBookQuery)
 
@@ -56,7 +56,9 @@ def sleeper(num):
     while True:
         print("Sleeping for %d seconds" % num)
         time.sleep(num)
-        bitmex_book = get_api_bitmex_book(depth="25")
+        depth  = conf.BOOK_DEPTH
+        symbol = conf.SYMBOL
+        bitmex_book = get_api_bitmex_book(depth=depth, symbol=symbol)
         #print("bitmex_book",bitmex_book)
         if bitmex_book != None:
             process_bitmex_book(bitmex_book)
